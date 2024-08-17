54cc6061b16ae04dad5167cd4543fe73579ff933
Rafael Arias <45745870+rafael-ariascalles@users.noreply.github.com>
2024-08-17 16:55:12 +0000

    command line

--- login_services/authentication/src/lib.rs
+++ login_services/authentication/src/lib.rs
@@ -1,45 +1,159 @@
 use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+use std::path::Path;
+use std::fs;
 
-#[derive(Debug)]
-pub struct User {
-    pub name: String,
-    pub password: String,
-    pub role: UserRole,
-}
 
-impl User {
-    pub fn new(name: &str, password: &str, user_role: UserRole) -> User {
-        User {
-            name: name.to_string(),
-            password: password.to_string(),
-            role: user_role,
-        }
-    }
-}
-#[derive(Debug)]
-pub enum UserRole {
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub enum LoginRole {
     Admin,
     User,
 }
 
-pub fn get_users() -> HashMap<String,User> {
+pub enum LoginAction {
+    Role(LoginRole),
+    Denied,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
+
+impl User {
+    pub fn new(username: &str, password: &str, user_role: LoginRole) -> User {
+        User {
+            username: username.to_string(),
+            password: hash_password(password),
+            role: user_role,
+        }
+    }
+}
+
+pub fn hash_password(password: &str) -> String {
+    use sha2::{Sha256, Digest};
+    let mut hasher = Sha256::new();
+    hasher.update(password);
+    // hexadecimal representation of a Numer :X 
+    format!("{:X}", hasher.finalize())
+}
+
+pub fn get_default_users() -> HashMap<String,User> {
     let mut users: HashMap<String, User> = HashMap::new();
-    users.insert("admin".to_string(), User::new("admin", "admin", UserRole::Admin));
-    users.insert( "moni".to_string(), User::new("moni", "moni", UserRole::User));
+    users.insert("admin".to_string(), User::new("admin", "admin", LoginRole::Admin));
+    users.insert( "moni".to_string(), User::new("moni", "moni", LoginRole::User));
     users
 }
 
-pub fn save_user(user: HashMap<String, User>) {
 
+pub fn get_users() -> HashMap<String,User> {
+    let users_path: &Path = Path::new("users.json");
+    if users_path.exists() {
+        let users_json: String = fs::read_to_string(users_path).expect("Unable to read file");
+        let users: HashMap<String, User> = serde_json::from_str(&users_json).unwrap();
+        users
+    }else {
+        let users: HashMap<String, User> = get_default_users();
+        let users_json: String = serde_json::to_string(&users).unwrap();
+        fs::write(users_path, users_json).expect("Unable to write file");
+        users
+    }
 }
+
+pub fn save_users(users: HashMap<String, User>) {
+    let users_path: &Path = Path::new("users.json");
+    let users_json: String = serde_json::to_string(&users).unwrap();
+    fs::write(users_path, users_json).expect("Unable to write file");
+
+    
+}
+
+pub fn greet_user(name: &str) -> String {
+    format!("Hello, {name}!")
+}
+
+pub fn reverse_user_name(name: &str) -> String {
+    name.chars().rev().collect()
+}
+
+pub fn login(user : &str, password : &str) -> bool {
+    user.to_lowercase() == "alice" && password == "1234"
+}
+
+pub fn login_users(user : &str, password : &str) -> Option<LoginAction> {
+    let user: String = user.to_lowercase();
+    let password: String = hash_password(password);
+
+    let mut result: Option<LoginAction> = Some(LoginAction::Denied);
+    
+    let users:HashMap<String, User>  = get_users();
+    if let Some(userdb) = users.get(&user) {
+        if userdb.password == password {
+            result = Some(LoginAction::Role(userdb.role.clone()));
+        } else {
+            result = Some(LoginAction::Denied);
+        }
+    }
+    result
+}
+
+pub fn login_users_fixed(user : &str, password : &str) -> Option<LoginAction> {
+    let user: String = user.to_lowercase();
+    let mut result: Option<LoginAction> = Some(LoginAction::Denied);
+    
+    if user != "admin" && user != "alice" {
+        return None;
+    } 
+
+    if user == "admin" && password == "1234" {
+        result = Some(LoginAction::Role(LoginRole::Admin));
+    }else if user == "alice" && password == "1234" {
+        result = Some(LoginAction::Role(LoginRole::User));
+    }
+
+    result
+}
+
+pub fn read_line() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().to_string()
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let users = get_users();
-        assert_eq!(users.len(), 2);
+    fn test_greet_user() {
+        assert_eq!(greet_user("Alice"), "Hello, Alice!");
     }
+
+    #[test]
+    fn test_reverse_user_name() {
+        assert_eq!(reverse_user_name("Jose"), "esoJ");
+    }
+
+    #[test]
+    fn test_login() {
+        assert!(login("alice", "1234"));
+        assert!(login("Alice", "1234"));
+        assert!(!login("Alice-not", "12345"));
+        assert!(!login("Alice", "12345-not"));
+    }
+
+    #[test]
+    fn test_login_users() {
+        //assert_eq!(login_users("admin", "1234"), LoginAction::Admin);
+        //assert_eq!(login_users("alice", "1234"), LoginAction::User);
+        //assert_eq!(login_users("Alice", "1234"), LoginAction::User);
+        //assert_eq!(login_users("Alice-not", "12345"), LoginAction::Denied);
+    }
+
+
 }
+
+
--- login_services/login_manager/src/main.rs
+++ login_services/login_manager/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, Subcommand};
 use authentication::get_users;
+use authentication::save_users;
+use authentication::LoginRole;
+use authentication::User;
 
 #[derive(Parser, Debug)]
 #[command()]
@@ -12,7 +15,32 @@ struct Args {
 enum Command {
     /// List the information of the users
     List,
+    /// list of all users
     ListAll,
+    /// Add users to the system
+    Add {
+        /// The username of the user to add
+        username: String,
+        /// The password assigned to the user
+        password: String,
+        /// if the user is an admin
+        #[arg(long)]
+        admin: bool,
+    },
+}
+
+fn add_user(username: String, password: String, admin: bool) {
+    let mut users: std::collections::HashMap<String, User> = get_users();
+
+    let role: LoginRole = if admin {
+        LoginRole::Admin
+    } else {
+        LoginRole::User
+    };
+
+    let new_user = User::new(&username, &password, role);
+    users.insert(username, new_user);
+    save_users(users);
 }
 
 fn list_users() {
@@ -23,12 +51,14 @@ fn list_users() {
     users
         .iter()
         .for_each(|(_, user)|{
-            println!("{:<20}{:<20?}", user.name, user.role);
+            println!("{:<20}{:<20?}", user.username, user.role);
         });
 }
 
 fn main() {
     let cli: Args = Args::parse();
+
+
     match cli.command {
         Some(Command::List) => {
             list_users();
@@ -38,6 +68,9 @@ fn main() {
             println!("{}",lines);
             println!("the list of all users is 1");
             println!("{}",lines);
+        },
+        Some(Command::Add {username, password, admin}) => {
+            add_user(username, password, admin);
         },
         None => {
             print!("run --help for more information");
